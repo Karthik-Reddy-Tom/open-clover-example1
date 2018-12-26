45de04acefc05ee13f87b3fd28f5d97f8692f2a3
Karthik Reddy <karthikr@STI.COM>
2018-12-26 15:52:04 +0530

    added phase $ goals on ececutions configuration in pom

--- target/site/clover/treemap-dash-json.js
+++
@@ -1,5 +0,0 @@
-var treeMapJson = {"id":"Clover database Wed Dec 26 2018 15:20:34 IST0","name":"","data":{
-    "$area":0.0,"$color":-100.0,"title":" 0 Elements,  -  Coverage"},
-  "children":[]}
-;
-processTreeMapDashJson (treeMapJson);
