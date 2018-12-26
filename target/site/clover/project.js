f86c1dc22c418a257091a936af41e466e2c3b246
Karthik Reddy <karthikr@STI.COM>
2018-12-26 16:16:46 +0530

    added changes configuration in pom

--- target/site/clover/project.js
+++
@@ -1,2 +0,0 @@
-processClover (  {"children":[],"name":"Clover database Wed Dec 26 2018 16:03:49 IST","stats":{"AvgMethodComplexity":-1,"Complexity":0,"ErroneousTests":0,"FailingTests":0,"FilteredElements":-100,"PassingTests":0,"PcErroneousTests":-1,"PcFailingTests":-1,"PcPassingTests":-1,"TestExecutionTime":0,"Tests":0,"TotalChildren":0,"TotalPercentageCovered":-100,"TotalStatements":0,"UncoveredElements":0},"title":"open 0.0.1-SNAPSHOT"}
- ); 
