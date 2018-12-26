eb7b98a7ae1e568fb9e684f84deb4bac14a26d20
Karthik Reddy <karthikr@STI.COM>
2018-12-26 15:21:50 +0530

    added php configuration in pom

--- target/site/clover/treemap-json.js
+++ target/site/clover/treemap-json.js
@@ -1,4 +1,4 @@
-var treeMapJson = {"id":"Clover database Tue Dec 25 2018 18:36:02 IST0","name":"","data":{
+var treeMapJson = {"id":"Clover database Wed Dec 26 2018 15:20:34 IST0","name":"","data":{
     "$area":0.0,"$color":-100.0,"title":" 0 Elements,  -  Coverage"},
   "children":[]}
 ;
